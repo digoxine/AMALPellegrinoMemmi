eb4b92444f6d6e3acbb4e8f5f0116a84e569f419
arthur-pe <pellegrino.arthur@gmail.com>
2020-12-20 20:31:13 +0100

    TP7 updated

--- TME7/tp7.py
+++ TME7/tp7.py
@@ -13,6 +13,7 @@ from torch.utils.tensorboard import SummaryWriter
 import sentencepiece as spm
 import torch.nn.functional as F
 from tp7_preprocess import TextDataset
+import math
 
 # Utiliser tp7_preprocess pour générer le vocabulaire BPE et
 # le jeu de donnée dans un format compact
@@ -94,6 +95,8 @@ class CNN2(nn.Module):
 
         self.embedding = nn.Embedding(num_embeddings=vocab_dim, embedding_dim=embedding_dim)
 
+        self.activation = nn.ReLU()
+
         chan = embedding_dim
         for i in range(0, len(layers)):
             self.convs.append(nn.Conv1d(in_channels=chan, kernel_size=layers[0][0], stride=layers[i][1],
@@ -101,29 +104,73 @@ class CNN2(nn.Module):
             self.pools.append(nn.MaxPool1d(kernel_size=layers[i][0], stride=layers[i][1]))
             chan = layers[i][2]
 
+        self.linear = nn.Linear(chan, out_dim)
+        self.layers = layers
+
+        self.displacementLength()
 
     def forward(self, x):
-        """
-        x: (batch_size x len)
-        """
+
         x = self.embedding(x)
-        x = x.permute(0, 2, 1)  # (batch_size x num_channels = embdedding_dim x len)
+        x = x.permute(0, 2, 1)
+
         for i in range(len(self.convs)):
-            x = self.convs[i](x)
+            x = self.activation(self.convs[i](x))
             x = self.pools[i](x)
 
         x = torch.max(x, -1)[0]
-        #x = torch.sigmoid(x)
+        x = self.linear(x)
+
         return x
 
-def inIndexes(layers, index):
-    pass
+    def lenOut(self, seq_len):
 
+        for i in range(len(self.layers)):
 
+            seq_len = math.floor((seq_len-self.layers[i][0])/self.layers[i][1]+1)
+            seq_len = math.floor((seq_len-self.layers[i][0])/self.layers[i][1]+1)
+
+        return seq_len
+
+    def displacementLength(self):
+
+        s = 1
+        w = 0
+
+        for i in self.layers:
+            w += s*(i[0]-1)
+            s *= i[1]
+
+        self.s = s
+        self.w = w
+
+    def activations(self, x): #returns how much each element of sequence activates output
+
+        initial_len = x.size()[1]
+
+        x = self.embedding(x)
+        x = x.permute(0, 2, 1)
+
+        for i in range(len(self.convs)):
+            x = self.activation(self.convs[i](x))
+            x = self.pools[i](x)
+
+        x = x.permute(2,0,1)
+
+        act = [torch.zeros(x.size()[1:]).to(device) for i in range(initial_len)]
+
+        for i in range(len(x)):
+            for j in range(self.w):
+
+                act[i*self.s+j] += x[i]
+
+        act = torch.stack(act)
+
+        return act
 
 #kernel_size = size of n-gram, out_channels
-#model = CNN([(3,1,2), (2,1,2), (4,1,2)], 256, vocab_size, 3) #(kernel_size, stride, out_channels)
-model = CNN2([(5,2,100), (3,2,50)], 256, vocab_size, 2) #(kernel_size, stride, out_channels)
+#model = CNN([(3,1,10), (7,1,10), (5,1,10)], 256, vocab_size, 2) #(kernel_size, stride, out_channels)
+model = CNN2([(3,2,50), (3,1,10)], 256, vocab_size, 2) #(kernel_size, stride, out_channels)
 loss = nn.CrossEntropyLoss()
 optim = torch.optim.Adam(params=model.parameters(), lr=10**-4)
 
@@ -132,58 +179,88 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 loss.to(device)
 
-epochs = 10
 
-for epoch in range(epochs):
+model.activations(next(iter(train_iter))[0].to(device))
+exit()
 
-    train_loss = 0
-    n = 0
-    train_acc = 0
+def Train():
+    epochs = 10
+
+    for epoch in range(epochs):
+
+        train_loss = 0
+        n = 0
+        train_acc = 0
+        for x in train_iter:
+            n +=1
+
+            y = x[1].to(device)
+
+            yhat = model(x[0].to(device))
+
+            l = loss(yhat, y)
+
+            train_loss+=l.item()
+
+            train_acc += (torch.argmax(yhat, 1) == y).float().mean().item()
+
+            optim.zero_grad()
+            l.backward()
+            optim.step()
+
+        train_loss /= n
+        train_acc /= n
+
+        print('Train loss: ', train_loss, '\tTrain acc: ', train_acc)
+        writer.add_scalar('Loss/Train', train_loss, epoch)
+        writer.add_scalar('Acc/Train', train_acc, epoch)
+
+
+        val_loss = 0
+        n = 0
+        val_acc = 0
+        for x in val_iter:
+            n += 1
+
+            y = x[1].to(device)
+
+            yhat = model(x[0].to(device))
+
+            l = loss(yhat, y)
+
+            val_acc += (torch.argmax(yhat, 1) == y).float().mean().item()
+
+            val_loss += l.item()
+
+        val_loss /= n
+        val_acc /= n
+
+        print('Valid loss: ', val_loss, '\tValid acc: ', val_acc)
+        writer.add_scalar('Loss/Val', val_loss, epoch)
+        writer.add_scalar('Acc/Val', val_acc, epoch)
+        writer.flush()
+
+def activationsIdentification(len):
+    index_max_word = [0 for i in range(len)]
+    max_seq = [0 for i in range(len)]
+    max = [0 for i in range(len)]
+
     for x in train_iter:
-        n +=1
 
         y = x[1].to(device)
 
-        yhat = model(x[0].to(device))
+        yhat = model.activations(x[0].to(device))
 
-        l = loss(yhat, y)
+        yhat = yhat.squeeze()
+        for i in range(len):
+            if torch.max(yhat[:,i])[0]>max[i]:
+                max_seq[i] = yhat
+                index_max_word[i] = torch.argmax(yhat[:,i])
+                max[i] = torch.max(yhat[:,i])
 
-        train_loss+=l.item()
-
-        train_acc += (torch.argmax(yhat, 1) == y).float().mean().item()
-
-        optim.zero_grad()
-        l.backward()
-        optim.step()
-
-    train_loss /= n
-    train_acc /= n
-
-    print('Train loss: ', train_loss, '\tTrain acc: ', train_acc)
-    writer.add_scalar('Loss/Train', train_loss, epoch)
-    writer.add_scalar('Acc/Train', train_acc, epoch)
+    for i in range(len(index_max_word)):
 
 
-    val_loss = 0
-    n = 0
-    val_acc = 0
-    for x in val_iter:
-        n += 1
+activationsIdentification(10)
 
-        y = x[1].to(device)
 
-        yhat = model(x[0].to(device))
-
-        l = loss(yhat, y)
-
-        val_acc += (torch.argmax(yhat, 1) == y).float().mean().item()
-
-        val_loss += l.item()
-
-    val_loss /= n
-    val_acc /= n
-
-    print('Valid loss: ', val_loss, '\tValid acc: ', val_acc)
-    writer.add_scalar('Loss/Val', val_loss, epoch)
-    writer.add_scalar('Acc/Val', val_acc, epoch)
-    writer.flush()
