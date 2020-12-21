c68e44ec67c13db36d2d85e80c52cd2582b3059e
digoxine <sacha.memmi.etu@gmail.com>
2020-12-21 15:03:12 +0100

    modifs exo4 tme 4

--- TME4/exo4.py
+++ TME4/exo4.py
@@ -2,15 +2,30 @@ import string
 import unicodedata
 import torch
 import datetime
-from utils import RNN, device, DataTXT_All
+from utils import read_temps, RNN, device, DataCSV, nn, torch, DataCSV_All, State, DataTXT_All
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+import time
+from pathlib import Path
+
+
+
+
+
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 LETTRES = string.ascii_letters + string.punctuation+string.digits+' '
 id2lettre = dict(zip(range(1,len(LETTRES)+1),LETTRES))
 id2lettre[0]='' ##NULL CHARACTER
 lettre2id = dict(zip(id2lettre.values(),id2lettre.keys()))
+latent_size = 64
+sequence_pred_length = 20
+batch_size = 256
+number_classes = len(id2lettre)
 
-
-def Generate():
+def Generate(state):
     initial_sequence = 'this is unbelievable'
     h = torch.zeros(1, latent_size).to(device)
     x = torch.squeeze(one_hot(string2code(normalize(initial_sequence))[None, :], number_classes).to(device))
@@ -41,104 +56,103 @@ def code2string(t):
         t = t.tolist()
     return ''.join(id2lettre[i] for i in t)
 
-from utils import read_temps, RNN, device, DataCSV, nn, torch, DataCSV_All, State
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
-import numpy as np
-import time
-from pathlib import Path
 
 
-for sequence_length in range(5,20, 5):
-    for sequence_pred_length in range(1,3,1):
+
+def one_hot(ind, dic_length):
+    t = torch.zeros(len(ind), len(ind[0]), dic_length).to(device)
+    for j in range(len(ind)):
+
+        for i in range(len(ind[0])):
+            t[j][i][ind[j][i]] = 1
+    return t
+
+
+def inv_one_hot(pred):
+    arg_max = torch.argmax(pred).to(device).item()
+
+    return id2lettre[arg_max]
+
+
+def Train():
+
+
+    data_train = DataTXT_All('data/trump_full_speech.txt', sequence_length)
+    data = DataLoader(data_train, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
+    model = RNN(len(id2lettre), latent_size, len(id2lettre))
+
+    loss = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters(), lr=5 * 10 ** -3)
+
+    iterations = 15
+
+    # GPU
+    model.to(device)
+    loss.to(device)
+
+    writer = SummaryWriter("runs/exo4/runs_sequence_length" + str(sequence_length) + "seq_pred_len" + str(
+        sequence_pred_length) + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    savepath = Path("seq_gen" + "seq_len" + str(sequence_length) + "seq_pred_len" + str(sequence_pred_length) + str(
+        latent_size) + ".pch")
+    if savepath.is_file():
+        with savepath.open("rb") as fp:
+            state = torch.load(fp)
+    else:
+        state = State(model, optim)
+    state.optim.lr = 5 * 10 ** -3
+    for i in range(iterations):
+
+        train_loss = 0
+        nt = 0
+        for x in data:
+
+            if nt%1000==0:
+                print(nt, end=', ')
+
+            nt += 1
+
+            y = x.to(device)
+            x= one_hot(x, len(id2lettre)).to(device)
+            h = torch.zeros(batch_size, latent_size).to(device)
+            x = x.permute(1, 0, 2).to(device)
+
+            h = state.model(x, h).to(device)
+
+            yhat = state.model.decoder(h).transpose(0,1).to(device)
+
+            l = loss(yhat.flatten(0,1), y.flatten())
+
+            state.optim.zero_grad()
+            l.backward()
+            state.optim.step()
+
+            train_loss += l.data.to(device).item()
+
+        train_loss = batch_size*sequence_length*train_loss/nt
+
+        writer.add_scalar('Loss/Train', train_loss, i)
+        print()
+        print('Epoch: ', i+1, '\tError train: ', train_loss)
+        Generate(state)
+        with savepath.open("wb") as fp:
+            state.epoch = i+1
+            torch.save(state,fp)
+
+
+for sequence_length in range(20,25, 5):
 #sequence_length = 15
-#sequence_pred_length = 20
-        batch_size = 256
-
-        data_train = DataTXT_All('data/trump_full_speech.txt', sequence_length)
-        data = DataLoader(data_train, batch_size=batch_size, shuffle=True,drop_last=True)
-
-        latent_size = 64
-        number_classes = len(id2lettre)
-        model = RNN(len(id2lettre), latent_size, len(id2lettre))
-
-        loss = nn.CrossEntropyLoss()
-        optim = torch.optim.Adam(model.parameters(), lr=5*10**-3)
-
-        iterations = 15
-
-        #GPU
-        model.to(device)
-        loss.to(device)
-
-        writer = SummaryWriter("runs/exo4/runs_sequence_length"+str(sequence_length)+"seq_pred_len"+str(sequence_pred_length)+datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-        def one_hot(ind, dic_length):
-            t = torch.zeros(len(ind), len(ind[0]), dic_length).to(device)
-            for j in range(len(ind)):
-
-                for i in range(len(ind[0])):
-                    t[j][i][ind[j][i]] = 1
-            return t
-
-        def inv_one_hot(pred):
-
-            arg_max = torch.argmax(pred).to(device).item()
+    Train()
 
 
-            return id2lettre[arg_max]
 
-        savepath = Path("seq_gen"+"seq_len"+str(sequence_length)+"seq_pred_len"+str(sequence_pred_length)+str(latent_size)+".pch")
-        if savepath.is_file():
-            with savepath.open("rb") as fp:
-                state = torch.load(fp)
-        else:
-            state = State(model, optim)
 
-        state.optim.lr=5*10**-3
-
-        def Train():
-            for i in range(iterations):
-
-                train_loss = 0
-                nt = 0
-                for x in data:
-
-                    if nt%1000==0:
-                        print(nt, end=', ')
-
-                    nt += 1
-
-                    y = x.to(device)
-                    x= one_hot(x, len(id2lettre)).to(device)
-                    h = torch.zeros(batch_size, latent_size).to(device)
-                    x = x.permute(1, 0, 2).to(device)
-
-                    h = state.model(x, h).to(device)
-
-                    yhat = state.model.decoder(h).transpose(0,1).to(device)
-
-                    l = loss(yhat.flatten(0,1), y.flatten())
-
-                    state.optim.zero_grad()
-                    l.backward()
-                    state.optim.step()
-
-                    train_loss += l.data.to(device).item()
-
-                train_loss = batch_size*sequence_length*train_loss/nt
-
-                writer.add_scalar('Loss/Train', train_loss, i)
-                print()
-                print('Epoch: ', i+1, '\tError train: ', train_loss)
-                Generate()
-                with savepath.open("wb") as fp:
-                    state.epoch = i+1
-                    torch.save(state,fp)
 
 
 
         #Generate()
-        Train()
-        Generate()
-        writer.close()
+        #Train()
+        #Generate()
+        #writer.close()
